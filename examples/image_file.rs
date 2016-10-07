1464c47a117b60a5584701c284d398c4a26b5a5a
plyhuns <plyhuns@PC-PLYHUNS.Reval.com>
2016-10-07 13:59:06 +0200

    fmt

--- examples/image_file.rs
+++ examples/image_file.rs
@@ -30,17 +30,17 @@ fn process_image(file: &str) {
         let img = image::load(BufReader::new(File::open(file).unwrap()), image::JPEG).unwrap().to_rgba();
         let data = img.into_vec();
 
-		// Here we extract the quantized colors from the image.
-		// We need no more than 16 colors (QUANT_SIZE).
+        // Here we extract the quantized colors from the image.
+        // We need no more than 16 colors (QUANT_SIZE).
         MMCQ::from_pixels_u8_rgba(data.as_slice(), QUANT_SIZE)
     };
 
-	// A `Vec` of colors, descendantely sorted by usage frequency  
+    // A `Vec` of colors, descendantely sorted by usage frequency
     let qc = mcq.get_quantized_colors();
     // println!("Quantized {:?}", qc);
-    
+
     // =============================================================================================
-    // Here we will demonstrate the extracted colors by generating the image 
+    // Here we will demonstrate the extracted colors by generating the image
     // that consists of both original image and a resulted palette.
     // =============================================================================================
 
@@ -68,7 +68,10 @@ fn process_image(file: &str) {
         }
     }
 
-    let ref mut fout = File::create(format!("./target/{}.png", Path::new(file).file_name().unwrap().to_str().unwrap()) .as_str()).unwrap();
+    let ref mut fout = File::create(format!("./target/{}.png",
+                                            Path::new(file).file_name().unwrap().to_str().unwrap())
+            .as_str())
+        .unwrap();
 
     let _ = image::ImageRgba8(imgbuf).save(fout, image::PNG);
 }
